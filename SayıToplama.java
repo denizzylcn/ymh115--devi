17881138403bce1277d5160dafc909078bcd883a
denizzylcn <117167542+denizzylcn@users.noreply.github.com>
2022-12-08 13:10:23 +0300

    Add files via upload

---
+++ SayıToplama.java
@@ -0,0 +1,15 @@
+
+public class SayıToplama {
+	//1'den 100'e kadar olan sayıların toplamı
+	public static void main(String[]args) {
+		int i;
+		int toplam =0;
+		for(i=1;i<100;i++){
+			toplam =toplam+i;
+		}
+	    System.out.println("1-100 arası sayıların toplamı= " + toplam);
+	
+	
+	}
+
+}
