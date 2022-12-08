0d5386d4556e92311337aa1e2bb125e526e6937c
denizzylcn <117167542+denizzylcn@users.noreply.github.com>
2022-12-08 13:06:48 +0300

    Add files via upload

---
+++ Ehliyethesaplama.java
@@ -0,0 +1,12 @@
+
+public class Ehliyethesaplama {
+	public static void main(String[]args) {
+    //Yaş 16'dan büyük ise "Ehliyet Alabilir", küçük ise "Ehliyet Alamaz" yazdıran programlama
+	int yas=32;
+	if(yas>=18) {
+       System.out.println("Ehliyet Alabilirsin");
+	}else {
+	   System.out.println("Ehliyet Alamaz");
+	}
+    }
+}
